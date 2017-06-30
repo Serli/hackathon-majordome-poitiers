f112cb9d78c92c83149bf48df371ec47b697d0da
Mickaël Boudaud <mickael.boudaud@serli.com>
2017-06-30 22:18:13 +0200

    Création d'une capture à partir d'un flux vidéo.

--- app/src/components/Stream/Stream.script.js
+++ app/src/components/Stream/Stream.script.js
@@ -1,17 +1,25 @@
 import config from '../../config'
 
 export default  {
-    name: 'rtc',
+  name: 'rtc',
 
-    methods: {
-        connect()
-        {
-            easyrtc.setSocketUrl(config.server.url)
-            easyrtc.easyApp("easyrtc.videoChatHd", "selfVideo", ["callerVideo"], this.loginSuccess, this.loginFailure)
-        },
+  methods: {
+    connect() {
+      easyrtc.setSocketUrl(config.server.url);
+      easyrtc.easyApp("easyrtc.videoChatHd", "selfVideo", ["callerVideo"], this.loginSuccess, this.loginFailure);
     },
+    capture() {
+      const video = this.$refs.video;
+      const canvas = document.createElement("canvas");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    mounted() {
-        this.connect()
+      window.open(canvas.toDataURL());
     }
+  },
+
+  mounted() {
+    this.connect()
+  }
 }
--- app/src/components/Stream/Stream.vue
+++ app/src/components/Stream/Stream.vue
@@ -2,8 +2,9 @@
     <div>
         <div id="videos">
             <video autoplay="autoplay" id="callerVideo"></video>
-            <video class="easyrtcMirror" autoplay="autoplay" id="selfVideo" muted="true" volume="0"></video>
+            <video class="easyrtcMirror" autoplay="autoplay" id="selfVideo" muted="true" volume="0" ref="video"></video>
         </div>
+      <button @click="capture()">Capture</button>
     </div>
 </template>
 <script src="./Stream.script.js"></script>
