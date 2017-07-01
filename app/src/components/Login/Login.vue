573441ac92ca08b723fd08546d2063dc13f0728d
whax <whax@localhost.localdomain>
2017-07-01 11:46:51 +0200

    surveiller

--- app/src/components/Login/Login.vue
+++ app/src/components/Login/Login.vue
@@ -10,22 +10,22 @@
             </button>
             <!--<br/>Connectez-vous à une surveillance existante<br/>-->
             <hr id="login-sep" class="mtm mbs"/>
-            <form method="post" id="login-form" class="w90 center mtm" @submit.prevent="catchSubmit">
-                Rejoindre une surveillance
-                <input type="text" name="user_id" class="pas plm prm mbm mtm txtcenter" placeholder="Identifiant"/>
-                <button class="pas prm plm w100" type="submit">
-                    Se connecter
-                </button>
-            </form>
-            <!--<button class="pas prm plm mtm w90 center">-->
-                <!--<router-link :to="{-->
-                    <!--name: 'listen',-->
-                    <!--params: {-->
-                        <!--room: 'default'-->
-                    <!--}-->
-                <!--}">Surveiller-->
-                <!--</router-link>-->
-            <!--</button>-->
+            <!--<form method="post" id="login-form" class="w90 center mtm" @submit.prevent="catchSubmit">-->
+                <!--Rejoindre une surveillance-->
+                <!--<input type="text" name="user_id" class="pas plm prm mbm mtm txtcenter" placeholder="Identifiant"/>-->
+                <!--<button class="pas prm plm w100" type="submit">-->
+                    <!--Se connecter-->
+                <!--</button>-->
+            <!--</form>-->
+            <button class="pas prm plm mts w90 center">
+                <router-link :to="{
+                    name: 'listen',
+                    params: {
+                        room: 'default'
+                    }
+                }">Surveiller
+                </router-link>
+            </button>
         </div>
     </div>
 </template>
