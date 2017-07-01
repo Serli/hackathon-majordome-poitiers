8d00f6b21c9993cb4603cf5b95be142b8227f6ac
flohhh <fl_w@live.fr>
2017-07-01 08:18:49 +0200

    re mega commit

--- api-rest/routes/image.js
+++ api-rest/routes/image.js
@@ -33,8 +33,8 @@ const fact = (io) => {
       Jimp.read(image.data).then(img => {
         const x = zoneIdObj.x;
         const y = zoneIdObj.y;
-        const w = img.bitmap.width * 0.2;
-        const h = img.bitmap.height * 0.2;
+        const w = img.bitmap.width * 0.333;
+        const h = img.bitmap.height * 0.333;
         comparator = factory(x, y, w, h);
         console.log('x :' + x + ' y :' + y + ' width: ' + w + 'height: ' + h);
         comparator.setOriginal(image.data, (err, cb) => {
@@ -54,61 +54,26 @@ const fact = (io) => {
   function getPercentageCrop(zoneId) {
     console.log('get percentage zone id :', zoneId);
     switch (zoneId) {
-      case '1' :
-        return {x: 0, y: 0};
-      case '2' :
-        return {x: 20, y: 0};
-      case '3' :
-        return {x: 40, y: 0};
-      case '4' :
-        return {x: 60, y: 0};
-      case '5' :
-        return {x: 80, y: 0};
+        case 1 :
+            return { x: 0, y: 0 };
+        case 2 :
+            return { x: 33, y: 0 };
+        case 3 :
+            return { x: 66, y: 0 };
 
-      case '6' :
-        return {x: 0, y: 20};
-      case '7' :
-        return {x: 20, y: 20};
-      case '8' :
-        return {x: 40, y: 20};
-      case '9' :
-        return {x: 60, y: 20};
-      case '10' :
-        return {x: 80, y: 20};
+        case 4 :
+            return { x: 0, y: 33 };
+        case 5 :
+            return { x: 33, y: 33 };
+        case 6 :
+            return { x: 66, y: 33 };
 
-      case '11' :
-        return {x: 0, y: 40};
-      case '12' :
-        return {x: 20, y: 40};
-      case '13' :
-        return {x: 40, y: 40};
-      case '14' :
-        return {x: 60, y: 40};
-      case '15' :
-        return {x: 80, y: 40};
-
-
-      case '16' :
-        return {x: 0, y: 60};
-      case '17' :
-        return {x: 20, y: 60};
-      case '18' :
-        return {x: 40, y: 60};
-      case '19' :
-        return {x: 60, y: 60};
-      case '20' :
-        return {x: 80, y: 60};
-
-      case '21' :
-        return {x: 0, y: 80};
-      case '22' :
-        return {x: 20, y: 80};
-      case '23' :
-        return {x: 40, y: 80};
-      case '24' :
-        return {x: 60, y: 80};
-      case '25' :
-        return {x: 80, y: 80};
+        case 7 :
+            return { x: 0, y: 66 };
+        case 8 :
+            return { x: 33, y: 66 };
+        case 9 :
+            return { x: 66, y: 66 };
 
 
     }
